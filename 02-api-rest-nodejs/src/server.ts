31335e50cb52f8c95eb4b483853b97d16a8a4e57
Marcos Corsi <marcosvcorsi@gmail.com>
2023-02-08 22:25:48 -0300

    feat: add 02 api rest

---
+++ 02-api-rest-nodejs/src/server.ts
@@ -0,0 +1,29 @@
+// type User = {
+//   birthYear: number;
+// };
+
+// function calculateAgeOfUser(user: User) {
+//   return new Date().getFullYear() - user.birthYear;
+// }
+
+// // calculateAgeOfUser("Diego");
+// // calculateAgeOfUser({});
+// const result = calculateAgeOfUser({
+//   birthYear: 1991,
+// });
+
+// console.log(result);
+
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", () => {
+  return "Hello World";
+});
+
+const port = Number(process.env.PORT ?? 3333);
+
+app
+  .listen({ port })
+  .then(() => console.log(`Server is running on port ${port}`));
